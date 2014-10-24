f666a2757309376646122714d1b24df31be8dda6
unknown <segottlieb@697-334-Sauron.sf.k12.internal>
2014-10-24 10:22:45 -0700

    Created Program

--- build-tmp/source/OldMacDonald.java
+++ build-tmp/source/OldMacDonald.java
@@ -1,4 +1,20 @@
-void setup()
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class OldMacDonald extends PApplet {
+
+public void setup()
 {
 	Farm f = new Farm();
 	f.animalSounds();
@@ -67,7 +83,7 @@ class Chick implements Animal
 	public Chick(String type, String sound, String sound2)    
 	{         
 		myType = type;
-		if(Math.random() > 0.5)
+		if(Math.random() > 0.5f)
 		{
 			mySound = sound;
 		}
@@ -90,4 +106,13 @@ class Pig implements Animal
 	}     
 	public String getSound(){return mySound;}     
 	public String getType(){return myType;} 
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "OldMacDonald" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
